77a567640f22769cd5191242adf5203c1afdfac9
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-09-06 15:16:16 +0200

    create table of weatherDetails

--- src/app/weather/weather-details/weather-details.component.ts
+++ src/app/weather/weather-details/weather-details.component.ts
@@ -4,18 +4,35 @@ import { DailyWeatherInfo } from 'src/app/shared/daily-weather-info.model';
 @Component({
   selector: 'app-weather-details',
   templateUrl: './weather-details.component.html',
-  styleUrls: ['./weather-details.component.css']
+  styleUrls: ['./weather-details.component.css'],
 })
 export class WeatherDetailsComponent {
   @Input() weatherDetails: DailyWeatherInfo = {
-    date : new Date(),
-    weatherCode: "Cloudy",
-    weatherImage: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    date: new Date(),
+    weatherCode: 'Cloudy',
+    weatherImage: 'https://ssl.gstatic.com/onebox/weather/64/cloudy.png',
     maxTemperature: 41.2,
     maxApparentTemperature: 38.2,
     minTemperature: 13.4,
     minApparentTemperature: 20.3,
     snowProbability: 0.0,
     rainProbability: 0.1,
+  };
+
+  weatherDetailsArray: { key: string; value: number }[] = [];
+
+  constructor() {
+    this.weatherDetailsArray = [
+      { key: 'Max Temperature', value: this.weatherDetails.maxTemperature },
+      {
+        key: 'Max apparent Temperature',
+        value: this.weatherDetails.maxApparentTemperature,
+      },
+      { key: 'Min Temperature', value: this.weatherDetails.minTemperature },
+      {
+        key: 'Min apparent Temperature',
+        value: this.weatherDetails.minApparentTemperature,
+      },
+    ];
   }
 }
